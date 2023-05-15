a64b9ea5135edea327397b607cfe526f0bb405e9
FreeHi <freehi@cumallover.me>
2023-05-15 11:48:54 +0800

    Added Cors origin options

--- express-modules/create-server.ts
+++ express-modules/create-server.ts
@@ -1,12 +1,17 @@
 import chalk from 'chalk';
+import cors from 'cors'
 import express, { Express } from 'express';
 import { Config } from '../typescript/interfaces'
 import routes from './routes'
 const server: Express = express();
 
+const corsOptions = {
+    origin: '*'
+}
 
 const initServer = (config: Config) => {
     // server.use(express.static('public'));
+    server.use(cors(corsOptions))
     server.use('/hk-transport', routes);
     server.listen(config.server.port, () => {
         console.info(chalk.green(`[server] listening on port ${config.server.port}`))
