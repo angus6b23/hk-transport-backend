57812fee616a347e659b2fd900bc8456fd501f3c
rippka <rippka@tfwno.gf>
2023-06-06 22:37:46 +0800

    Will now implement bus routes operated by different companies

--- scrape/bus.ts
+++ scrape/bus.ts
@@ -115,15 +115,15 @@ const implementKMB = async (buses: BusRoute[]): Promise<BusRoute[]> => {
 const implementCTB = async (buses: BusRoute[]): Promise<BusRoute[]> => {
     axiosThrottle.use(axios, { requestsPerSecond: 10 });
     const ctbBuses = buses.filter(bus => bus.company.length == 1 && (bus.company.includes('CTB') || bus.company.includes('NWFB')));
+    const mixedBuses = buses.filter(bus => bus.company.length >= 2);
     console.info(chalk.blue(`[bus] Now implementing CTB routes`))
     try {
         // Create Array for fetching all route data
         const ctbIdReq = ctbBuses.map(async (ctbBus) => {
             const company =
-                (ctbBus.company.includes('CTB')) ? 'CTB' :
-                    (ctbBus.company.includes('NWFB')) ? 'NWFB' : null;
+               (ctbBus.company.includes('CTB')) ? 'CTB' :
+               (ctbBus.company.includes('NWFB')) ? 'NWFB' : null;
             const direction = (ctbBus.direction == 1) ? 'outbound' : 'inbound'; //Direction 1 = outbound, 2 = inbound
-            await sleep(100);
             return axios(`https://rt.data.gov.hk/v1.1/transport/citybus-nwfb/route-stop/${company}/${ctbBus.routeNo}/${direction}`)
         })
         // Fetch all routes,
@@ -157,19 +157,43 @@ const implementCTB = async (buses: BusRoute[]): Promise<BusRoute[]> => {
                 newStopList.push(newStop);
             }
             ctbBuses[i].stops = newStopList
-            // Note: CTB api inbound and outbound is not consistent with Gov Geojson
-            // Find CTB with corresponding origin and route No
-            /* Old code, not working
-            const checkIndex = ctbBuses.findIndex(ctbBus => ctbBus.routeNo === targetRoute[0].route && newStopList[0].nameTC.includes(ctbBus.originTC)  || ctbBus.originTC.includes(newStopList[0].nameTC.slice(0, 2)));
-            if (checkIndex == -1) {
-                console.log(chalk.red(`Route not found: ${ctbBuses[i].routeNo} : ${ctbBuses[i].originTC} =/= ${newStopList[0].nameTC}`));
-                warningCount.routeWarn += 1;
-                ctbBuses[i].stops = newStopList;
+        }
+        // Grab all Mixed bus data
+        // Even KMB and CTB / NWFB operate the same route, the direction use different internally
+        console.info(chalk.blue(`[bus] Now fetching buses with several companies`));
+        const mixedBusIdReq = mixedBuses.map(bus => {
+            const company =
+                (bus.company.includes('CTB')) ? 'CTB' :
+                (bus.company.includes('NWFB')) ? 'NWFB' : null;
+            const direction = (bus.direction == 1) ? 'outbound' : 'inbound'; //Direction 1 = outbound, 2 = inbound
+            return axios(`https://rt.data.gov.hk/v1.1/transport/citybus-nwfb/route-stop/${company}/${bus.routeNo}/${direction}`)
+        });
+        const mixedBusIdRes = await axios.all(mixedBusIdReq)
+        for (let i = 0; i < mixedBusIdRes.length; i++){
+            let idRes = mixedBusIdRes[i].data.data
+            let firstStopReq = await axios(`https://rt.data.gov.hk/v1.1/transport/citybus-nwfb/stop/${idRes[0].stop}`);
+            let firstStopName = firstStopReq.data.data.name_en.slice(0,5).toLowerCase();
+            if (mixedBuses[i].stops[0].nameEN.toLowerCase().includes(firstStopName)){ //Simply populate altId if the name of first stop matches
+                console.log(chalk.grey(`[bus] Successfully find bus ${idRes[0].route}`));
+                for (let j = 0; j < idRes.length; j++){
+                    if (mixedBuses[i].stops[j]){
+                        mixedBuses[i].stops[j].altId = idRes[j].stop
+                    }
+                }
             } else {
-                console.log(chalk.grey(`Found: ${ctbBuses[checkIndex].routeNo} : ${ctbBuses[checkIndex].originTC} = ${newStopList[0].nameTC} => ${ctbBuses[checkIndex].destTC}`));
-                ctbBuses[checkIndex].stops = newStopList
+                // otherwise, find another route with same routen no and matching first stop
+                let findIndex = mixedBuses.findIndex(bus => bus.routeNo == idRes[0].route && bus.stops[0].nameEN.toLowerCase().includes(firstStopName));
+                if (findIndex != -1){
+                    console.log(chalk.grey(`[bus] Using alternative route for ${idRes[0].route}`));
+                    for (let j = 0; j < idRes.length; j++){
+                        if (mixedBuses[findIndex].stops[j]){
+                            mixedBuses[findIndex].stops[j].altId = idRes[j].stop
+                        }
+                    }
+                } else {
+                    console.warn(chalk.yellow(`[bus] Unable to find bus for route ${idRes[0].route}`));
+                }
             }
-            */
         }
     } catch (err) {
         console.error(chalk.red(`[bus] Error: Implementing CTB API - ${err}`))
@@ -179,89 +203,89 @@ const implementCTB = async (buses: BusRoute[]): Promise<BusRoute[]> => {
     return buses
 }
 /*
-const implementNLB = async (buses: BusRoute[]): Promise<BusRoute[]> => {
-    const checkParenthesis = /\(.*$/; // Remove parenthesis due to different naming
-    try {
-        console.info(chalk.blue(`[bus] Now implementing NLB routes`))
-        const nlbResponse = await axios('https://rt.data.gov.hk/v2/transport/nlb/route.php?action=list');
-        const nlbRoutes = nlbResponse.data.routes
-        const originRegex = /^.*\>/;
-        const destRegex = /\>.*$/;
-        for (const route of nlbRoutes) { //Loop for nlb Routes and try to find route with same routeNo, origin and destination
-            // Route name of nlb is named as `${origin} > ${dest}`
-            const origin = route.routeName_e.match(originRegex)[0].replace(' >', '').replace(checkParenthesis, '');
-            const dest = route.routeName_e.match(destRegex)[0].replace('> ', '').replace(checkParenthesis, '');
-            const checkIndex = buses.findIndex(bus => bus.company.includes('NLB') && bus.routeNo == route.routeNo && bus.destEN.replaceAll(' ', '').includes(dest.replaceAll(' ', '')) && bus.originEN.replaceAll(' ', '').includes(origin.replaceAll(' ', '')));
-            if (checkIndex != -1) {
-                buses[checkIndex].altId = route.routeId;
-            } else {
-                // If not found, try to reduce the restraints to bind altId
-                const checkIndex = buses.findIndex(bus => bus.routeNo === route.routeNo && bus.company.includes('NLB'));
-                if (checkIndex != -1) {
-                    buses[checkIndex].altId = route.routeId;
-                } else {
-                    // Create NLB route if not found
-                    const newNlbRoute: BusRoute = {
-                        company: ['NLB'],
-                        type: 'bus',
-                        routeId: 'nlb' + route.routeId,
-                        altId: route.routeId,
-                        routeNo: route.routeNo,
-                        serviceMode: 'S',
-                        specialType: 0,
-                        infoLinkEN: 'https://www.nlb.com.hk/route?q=' + route.routeNo,
-                        infoLinkTC: 'https://www.nlb.com.hk/route?q=' + route.routeNo,
-                        fullFare: '0',
-                        direction: 1,
-                        journeyTime: 0,
-                        destTC: route.routeName_c.match(destRegex)[0].replace('> ', ''),
-                        destEN: route.routeName_e.match(destRegex)[0].replace('> ', ''),
-                        originTC: route.routeName_c.match(originRegex)[0].replace(' >', ''),
-                        originEN: route.routeName_e.match(originRegex)[0].replace(' >', ''),
-                        starred: false,
-                        stops: []
-                    }
-                    // Pull stops of specific route from NLB API
-                    const { data: newRouteStops } = await axios(`https://rt.data.gov.hk/v2/transport/nlb/stop.php?action=list&routeId=${route.altId}`);
-                    for (let i = 0; i < newRouteStops.stops.length; i++) {
-                        const newStop: BusStop = {
-                            nameTC: newRouteStops.stops[i].stopName_c,
-                            nameEN: newRouteStops.stops[i].stopName_e,
-                            stopId: newRouteStops.stops[i].stopId,
-                            seq: i + 1,
-                            coord: [newRouteStops.stops[i].longitude, newRouteStops.stops[i].latitude],
-                            etas: []
-                        }
-                        newNlbRoute.stops.push(newStop);
-                    }
-                    buses.push(newNlbRoute);
-                }
-            }
-        }
-        // Supplement stop ids from NLB buses
-        const NLBBuses = buses.filter(route => route.company.includes('NLB'));
-        console.log(NLBBuses.length)
-        const stopReq = NLBBuses.map(route => axios(`https://rt.data.gov.hk/v2/transport/nlb/stop.php?action=list&routeId=${route.altId}`));
-        let stopData = await axios.all(stopReq);
-        for (let i = 0; i < NLBBuses.length; i++) {
-            let targetStops = NLBBuses[i].stops
-            if (stopData[i].data.stops.length == 0) {
-                console.log(`${NLBBuses[i].routeNo} with id ${NLBBuses[i].altId} not found`)
-                continue
-            }
-            for (let j = 0; j < targetStops.length; j++) {
-                if (stopData[i].data.stops[j]) {
-                    targetStops[j].altId = stopData[i].data.stops[j].stopId
-                } else {
-                    // console.log(`${NLBBuses[i].routeNo} stop ${j} not found`)
-                }
-            }
-        }
-        return buses
-    } catch (err) {
-        console.error(chalk.red(`[bus] Error while implementing NLB API: ${err}`));
-        return buses
+   const implementNLB = async (buses: BusRoute[]): Promise<BusRoute[]> => {
+   const checkParenthesis = /\(.*$/; // Remove parenthesis due to different naming
+   try {
+   console.info(chalk.blue(`[bus] Now implementing NLB routes`))
+   const nlbResponse = await axios('https://rt.data.gov.hk/v2/transport/nlb/route.php?action=list');
+   const nlbRoutes = nlbResponse.data.routes
+   const originRegex = /^.*\>/;
+   const destRegex = /\>.*$/;
+   for (const route of nlbRoutes) { //Loop for nlb Routes and try to find route with same routeNo, origin and destination
+                                    // Route name of nlb is named as `${origin} > ${dest}`
+                                    const origin = route.routeName_e.match(originRegex)[0].replace(' >', '').replace(checkParenthesis, '');
+                                    const dest = route.routeName_e.match(destRegex)[0].replace('> ', '').replace(checkParenthesis, '');
+                                    const checkIndex = buses.findIndex(bus => bus.company.includes('NLB') && bus.routeNo == route.routeNo && bus.destEN.replaceAll(' ', '').includes(dest.replaceAll(' ', '')) && bus.originEN.replaceAll(' ', '').includes(origin.replaceAll(' ', '')));
+                                    if (checkIndex != -1) {
+                                    buses[checkIndex].altId = route.routeId;
+                                    } else {
+// If not found, try to reduce the restraints to bind altId
+const checkIndex = buses.findIndex(bus => bus.routeNo === route.routeNo && bus.company.includes('NLB'));
+if (checkIndex != -1) {
+buses[checkIndex].altId = route.routeId;
+} else {
+// Create NLB route if not found
+const newNlbRoute: BusRoute = {
+company: ['NLB'],
+type: 'bus',
+routeId: 'nlb' + route.routeId,
+altId: route.routeId,
+routeNo: route.routeNo,
+serviceMode: 'S',
+specialType: 0,
+infoLinkEN: 'https://www.nlb.com.hk/route?q=' + route.routeNo,
+infoLinkTC: 'https://www.nlb.com.hk/route?q=' + route.routeNo,
+fullFare: '0',
+direction: 1,
+journeyTime: 0,
+destTC: route.routeName_c.match(destRegex)[0].replace('> ', ''),
+destEN: route.routeName_e.match(destRegex)[0].replace('> ', ''),
+originTC: route.routeName_c.match(originRegex)[0].replace(' >', ''),
+originEN: route.routeName_e.match(originRegex)[0].replace(' >', ''),
+starred: false,
+stops: []
+}
+// Pull stops of specific route from NLB API
+const { data: newRouteStops } = await axios(`https://rt.data.gov.hk/v2/transport/nlb/stop.php?action=list&routeId=${route.altId}`);
+for (let i = 0; i < newRouteStops.stops.length; i++) {
+const newStop: BusStop = {
+nameTC: newRouteStops.stops[i].stopName_c,
+nameEN: newRouteStops.stops[i].stopName_e,
+stopId: newRouteStops.stops[i].stopId,
+seq: i + 1,
+coord: [newRouteStops.stops[i].longitude, newRouteStops.stops[i].latitude],
+etas: []
+}
+newNlbRoute.stops.push(newStop);
+}
+buses.push(newNlbRoute);
+}
+}
+}
+// Supplement stop ids from NLB buses
+const NLBBuses = buses.filter(route => route.company.includes('NLB'));
+console.log(NLBBuses.length)
+const stopReq = NLBBuses.map(route => axios(`https://rt.data.gov.hk/v2/transport/nlb/stop.php?action=list&routeId=${route.altId}`));
+let stopData = await axios.all(stopReq);
+for (let i = 0; i < NLBBuses.length; i++) {
+let targetStops = NLBBuses[i].stops
+if (stopData[i].data.stops.length == 0) {
+console.log(`${NLBBuses[i].routeNo} with id ${NLBBuses[i].altId} not found`)
+continue
+}
+for (let j = 0; j < targetStops.length; j++) {
+    if (stopData[i].data.stops[j]) {
+        targetStops[j].altId = stopData[i].data.stops[j].stopId
+    } else {
+        // console.log(`${NLBBuses[i].routeNo} stop ${j} not found`)
     }
+}
+}
+return buses
+} catch (err) {
+    console.error(chalk.red(`[bus] Error while implementing NLB API: ${err}`));
+    return buses
+}
 }
 */ 
 
@@ -287,14 +311,14 @@ const implementNLB = async(buses:BusRoute[]): Promise<BusRoute[]> => {
                 destTC: route.routeName_c.match(destRegex)[0].replace('> ', ''),
                 destEN: route.routeName_e.match(destRegex)[0].replace('> ', ''),
                 infoLinkEN: `https://www.nlb.com.hk/route/detail/${route.routeId}`,
-                infoLinkTC: `https://www.nlb.com.hk/route/detail/${route.routeId}`,
-                starred: false,
+                    infoLinkTC: `https://www.nlb.com.hk/route/detail/${route.routeId}`,
+                    starred: false,
                 stops: []
             }
             NLBBuses.push(newRoute);
         }
         const NLBStopsReq = NLBBuses.map(bus => axios(`https://rt.data.gov.hk/v2/transport/nlb/stop.php?action=list&routeId=${bus.routeId}`))
-        const NLBStopData = await axios.all(NLBStopsReq);
+            const NLBStopData = await axios.all(NLBStopsReq);
         for (let i = 0; i < NLBStopData.length; i++){
             let data = NLBStopData[i].data.stops;
             let stops: BusStop[] = [];
@@ -319,39 +343,39 @@ const implementNLB = async(buses:BusRoute[]): Promise<BusRoute[]> => {
     }
 }
 /*
-const implementMTR = async (buses: BusRoute[]): Promise<BusRoute[]> => {
-    try {
-        console.info(chalk.blue('[bus] Now implementing MTR bus routes'))
-        const { data: mtrBusResponse } = await axios('https://opendata.mtr.com.hk/data/mtr_bus_stops.csv')
-        const mtrBusData: any[] = papa.parse(mtrBusResponse, PAPACONFIG).data;
-        let mtrBuses: BusRoute[] = buses.filter(bus => (bus.routeNo.indexOf('K') == 0 && bus.routeNo.length <= 5) || bus.company.includes('LRTFeeder'))
-        mtrBuses.forEach(route => {
-            route.company = ['LRTFeeder']
-        })
-        for (let item of mtrBusData) {
-            let { ROUTE_ID: routeNo, DIRECTION: direction, STATION_ID: stationId, STATION_SEQNO: seq, STATION_LATITUDE: latitude, STATION_LONGITUDE: longitude, STATION_NAME_CHI: nameTC, STATION_NAME_ENG: nameEN } = item;
-            direction = (direction == 'I') ? 1 : 2;
-            let index = mtrBuses.findIndex(route => route.routeNo == routeNo && route.direction == direction);
-            if (index == -1) {
-                index = mtrBuses.findIndex(route => route.routeNo == routeNo && route.direction == 1)
-            }
-            let targetBus = mtrBuses[index];
-            targetBus.stops[seq - 1] = {
-                seq: seq,
-                coord: [longitude, latitude],
-                stopId: stationId,
-                nameTC: nameTC,
-                nameEN: nameEN,
-                etas: []
-            }
-        }
-        return buses;
-    } catch (err) {
-        console.error(chalk.red(`[bus] Error while implementing MTR buses API: ${err}`));
-        return buses
-    }
+   const implementMTR = async (buses: BusRoute[]): Promise<BusRoute[]> => {
+   try {
+   console.info(chalk.blue('[bus] Now implementing MTR bus routes'))
+   const { data: mtrBusResponse } = await axios('https://opendata.mtr.com.hk/data/mtr_bus_stops.csv')
+   const mtrBusData: any[] = papa.parse(mtrBusResponse, PAPACONFIG).data;
+   let mtrBuses: BusRoute[] = buses.filter(bus => (bus.routeNo.indexOf('K') == 0 && bus.routeNo.length <= 5) || bus.company.includes('LRTFeeder'))
+   mtrBuses.forEach(route => {
+   route.company = ['LRTFeeder']
+   })
+   for (let item of mtrBusData) {
+   let { ROUTE_ID: routeNo, DIRECTION: direction, STATION_ID: stationId, STATION_SEQNO: seq, STATION_LATITUDE: latitude, STATION_LONGITUDE: longitude, STATION_NAME_CHI: nameTC, STATION_NAME_ENG: nameEN } = item;
+   direction = (direction == 'I') ? 1 : 2;
+   let index = mtrBuses.findIndex(route => route.routeNo == routeNo && route.direction == direction);
+   if (index == -1) {
+   index = mtrBuses.findIndex(route => route.routeNo == routeNo && route.direction == 1)
+   }
+   let targetBus = mtrBuses[index];
+   targetBus.stops[seq - 1] = {
+seq: seq,
+coord: [longitude, latitude],
+stopId: stationId,
+nameTC: nameTC,
+nameEN: nameEN,
+etas: []
 }
-*/
+}
+return buses;
+} catch (err) {
+console.error(chalk.red(`[bus] Error while implementing MTR buses API: ${err}`));
+return buses
+}
+}
+ */
 
 const implementMTR = async (buses: BusRoute[]): Promise<BusRoute[]> => {
     try {
@@ -387,8 +411,8 @@ const implementMTR = async (buses: BusRoute[]): Promise<BusRoute[]> => {
                     destEN: '',
                     destTC: '',
                     infoLinkEN: `https://www.mtr.com.hk/en/customer/services/searchBusRouteDetails.php?routeID=${routeNo}`,
-                    infoLinkTC: `https://www.mtr.com.hk/ch/customer/services/searchBusRouteDetails.php?routeID=${routeNo}`,
-                    stops: [newStop],
+                        infoLinkTC: `https://www.mtr.com.hk/ch/customer/services/searchBusRouteDetails.php?routeID=${routeNo}`,
+                        stops: [newStop],
                     starred: false
                 }
                 mtrBuses.push(newRoute);
